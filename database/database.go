f9c8c4e56eca73a674fa867523abccb854ed2e70
markmumba <markmumba01@gmail.com>
2024-07-29 20:53:52 +0300

    only using database url

--- database/database.go
+++ database/database.go
@@ -1,7 +1,6 @@
 package database
 
 import (
-	"fmt"
 	// "github.com/joho/godotenv"
 	"log"
 	"os"
@@ -11,34 +10,21 @@ import (
 )
 
 var DB *gorm.DB
-
 func ConnectDB() {
-
 	var err error
-	// err = godotenv.Load()
-	// if err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
-	dbHost := os.Getenv("PGHOST")
-	dbPort := os.Getenv("PGPORT")
-	dbUser := os.Getenv("PGUSER")
-	dbPassword := os.Getenv("PGPASSWORD")
-	dbName := os.Getenv("PGDATABASE")
 
-	// Print out the values for debugging (exclude password)
-	
-	if dbHost == "" || dbPort == "" || dbUser == "" || dbPassword == "" || dbName == "" {
-		log.Fatal("Database connection information is incomplete")
-	}
+	// Get the DATABASE_URL from environment variable
+	databaseURL := os.Getenv("DATABASE_URL")
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
-		dbHost, dbUser, dbPassword, dbName, dbPort)
-	if dsn == "" {
+	if databaseURL == "" {
 		log.Fatal("DATABASE_URL environment variable is not set")
 	}
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	// Open a connection to the database
+	DB, err = gorm.Open(postgres.Open(databaseURL), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Error connecting to database: ", err)
 	}
+
 	log.Println("Connection established successfully")
 }
