ac7d55f9439298da0bae8020e0e4b6f6cfdbbe1c
markmumba <markmumba01@gmail.com>
2024-07-30 15:26:43 +0300

    removing comments

--- database/database.go
+++ database/database.go
@@ -13,24 +13,17 @@ var DB *gorm.DB
 func ConnectDB() {
 	var err error
 
-	// Check if we're in a production environment
 	if os.Getenv("GO_ENV") != "production" {
-		// Load .env file only in non-production environments
 		err = godotenv.Load()
 		if err != nil {
 			log.Println("Warning: Error loading .env file:", err)
-			// Don't fatal here, as the environment variables might be set another way
 		}
 	}
 
-	// Get the DATABASE_URL from environment variable
 	databaseURL := os.Getenv("DATABASE_URL")
-
 	if databaseURL == "" {
 		log.Fatal("DATABASE_URL environment variable is not set")
 	}
-
-	// Open a connection to the database
 	DB, err = gorm.Open(postgres.Open(databaseURL), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Error connecting to database: ", err)
