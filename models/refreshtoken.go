d2a4db996556bf16a510d36ff6829771dbb853f9
markmumba <markmumba01@gmail.com>
2024-08-04 15:25:44 +0300

    wrong placement of type uuid in refresh token

--- models/refreshtoken.go
+++ models/refreshtoken.go
@@ -9,8 +9,8 @@ import (
 type RefreshToken struct {
 	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
 	Token     string    `gorm:"unique;not null"`
-	UserID    uuid.UUID `gorm:"not null"`
-	User      User      `gorm:"type:uuid;foreignKey:UserID"`
+	UserID    uuid.UUID `gorm:"type:uuid;not null"`
+	User      User      `gorm:"foreignKey:UserID"`
 	ExpiresAt time.Time `gorm:"not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
